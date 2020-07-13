7bb94044000132c2e4de104e6dedc4afb015762d
Chris Sykes <chrispsykes12@gmail.com>
2020-07-13 11:07:44 -1000

    webpack deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// change require to es6 import style
+// import $ from 'jquery';
+import './style.scss';
+
+let secs = 0;
+
+function updateSec() {
+  secs += 1;
+  document.getElementById('main').innerText = `You've been on this page for ${secs} seconds.`;
+}
+
+setInterval(updateSec, 1000);
